fb69aa3b2bfecc66be8de9086c2124814a200f19
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-11-22 19:36:26 +0300

    Add path score caching to state

--- src/main/kotlin/ru/spbstu/competition/game/State.kt
+++ src/main/kotlin/ru/spbstu/competition/game/State.kt
@@ -4,15 +4,50 @@ import ru.spbstu.competition.protocol.data.Map as Graph
 import ru.spbstu.competition.protocol.data.Claim
 import ru.spbstu.competition.protocol.data.River
 import ru.spbstu.competition.protocol.data.Setup
+import java.util.*
 
 enum class RiverState{ Our, Enemy, Neutral }
 
 class State {
     lateinit var graph: Graph
     lateinit var mineEntrances: Set<River>
+    lateinit var adjacentRivers: Map<Int, List<River>>
+
+    private data class Path(val mine: Int, val site: Int)
+    private val scoreCache = mutableMapOf<Path, Int>()
+
     val rivers = mutableMapOf<River, RiverState>()
     var mines = listOf<Int>()
     var myId = -1
+
+    fun neighbors(site: Int): Sequence<Int> =
+            (adjacentRivers[site] ?: emptyList()).asSequence().map {
+                if (it.source == site) it.target else it.source
+            }
+
+    private fun bfs(mine: Int) {
+        val visited = mutableMapOf<Int, Int>()
+        val queue = ArrayDeque<Int>()
+        visited[mine] = 0
+        queue += mine
+        scoreCache[Path(mine, mine)] = 0
+        while (queue.isNotEmpty()) {
+            val current = queue.poll()
+            val distance = visited[current]!!
+            for (neighbor in neighbors(current)) {
+                if (neighbor in visited) continue
+                visited[neighbor] = distance + 1
+                scoreCache[Path(mine, neighbor)] = (distance + 1) * (distance + 1)
+                queue += neighbor
+            }
+        }
+    }
+
+    private fun initCache() {
+        for (mine in mines) {
+            bfs(mine)
+        }
+    }
 
     fun init(setup: Setup) {
         myId = setup.punter
@@ -26,6 +61,13 @@ class State {
         mineEntrances = rivers.keys.filter { river ->
             river.source in mines || river.target in mines
         }.toSet()
+        val adjacentRivers = mutableMapOf<Int, MutableList<River>>()
+        for (river in rivers.keys) {
+            adjacentRivers.getOrPut(river.source) { mutableListOf() }.add(river)
+            adjacentRivers.getOrPut(river.target) { mutableListOf() }.add(river)
+        }
+        this.adjacentRivers = adjacentRivers
+        initCache()
     }
 
     fun update(claim: Claim) {
